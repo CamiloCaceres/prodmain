23cf246b26b3004983a4fbaf5b166a999889b62f
camilo caceres <camiloac97@gmail.com>
2024-07-23 22:56:02 -0500

    feat: add mobile menu

--- components/MainNav.vue
+++ components/MainNav.vue
@@ -1,33 +1,51 @@
 <script setup lang="ts">
-import { HamburgerMenuIcon } from '@radix-icons/vue';
+import { HamburgerMenuIcon } from "@radix-icons/vue";
 
-const links = reactive([
-  {
-    text: "Sobre nosotros",
-    to: "/nosotros",
-  },
-  {
-    text: "Contáctanos",
-    to: "/contacto",
-  },
-  {
-    text: "Servicios",
-    to: "/servicios",
-  },
+import { Home, Info, Briefcase, Mail, ChevronLeft } from "lucide-vue-next";
+
+const links = ref([
+  { text: "Sobre nosotros", to: "/nosotros", icon: Info },
+  { text: "Servicios", to: "/servicios", icon: Briefcase },
+  { text: "Contacto", to: "/contacto", icon: Mail },
 ]);
 </script>
 
 <template>
-  <nav class="flex justify-between sm:justify-around py-4 px-8 shadow-md items-center">
-    <h1>Prodmain</h1>
-    <div class="sm:flex items-center space-x-4 hidden">
+  <nav
+    class="flex justify-between sm:justify-around p-5 shadow-md items-center"
+  >
+    <Sheet>
+      <SheetTrigger>
+        <Button variant="outline" class="sm:hidden">
+          <HamburgerMenuIcon class="w-4 h-4" /> </Button
+      ></SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader class="mb-12 mx-auto">
+          <NuxtLink to="/">
+            <img src="/Logo.webp" alt="Prodmain" class="h-12 w-auto" />
+          </NuxtLink>
+        </SheetHeader>
+        <div class="flex flex-col justify-center items-start space-y-4">
+          <div v-for="(link, index) in links" :key="index">
+            <NuxtLink :to="link.to">
+              <Button class="text-md" variant="link"
+                >{{ link.text }}
+                <component :is="link.icon" class="w-5 h-5 ml-2"
+              /></Button>
+            </NuxtLink>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+    <NuxtLink to="/">
+      <img src="/Logo.webp" alt="Prodmain" class="h-12 w-auto" />
+    </NuxtLink>
+    <div class="hidden sm:flex items-center space-x-4">
       <div v-for="(link, index) in links" :key="index">
-        <Button variant="link">{{ link.text }}</Button>
+        <NuxtLink :to="link.to">
+          <Button class="text-md" variant="link">{{ link.text }}</Button>
+        </NuxtLink>
       </div>
     </div>
-
-    <Button variant="outline" class="sm:hidden">
-      <HamburgerMenuIcon class="w-4 h-4"/>
-    </Button>
   </nav>
 </template>
